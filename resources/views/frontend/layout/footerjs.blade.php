20d2b573e7dac69c2b63e4247513ded5d3a66e0a
SmSmBeRo <samehdesigner6@gmail.com>
2022-07-21 18:13:02 +0200

    sameh

--- resources/views/frontend/layout/footerjs.blade.php
+++ resources/views/frontend/layout/footerjs.blade.php
@@ -1,12 +1,16 @@
 <!-- JavaScript Libraries -->
-<script src="{{asset('frontend/js/jquery-3.4.1.min.js')}}"></script>
-<script src="{{asset('frontend/js/bootstrap.bundle.min.js')}}"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
+{{--<script src="{{asset('frontend/js/jquery-3.4.1.min.js')}}"></script>--}}
+<script src="{{asset('frontend/js/fancybox.js')}}"></script>
+{{--<script src="{{asset('frontend/js/bootstrap.bundle.min.js')}}"></script>--}}
 <script src="{{asset('frontend/lib/wow/wow.min.js')}}"></script>
 <script src="{{asset('frontend/lib/easing/easing.min.js')}}"></script>
 <script src="{{asset('frontend/lib/waypoints/waypoints.min.js')}}"></script>
 <script src="{{asset('frontend/lib/counterup/counterup.min.js')}}"></script>
 <script src="{{asset('frontend/lib/owlcarousel/owl.carousel.min.js')}}"></script>
-<script src="{{asset('frontend/js/fancybox.js')}}"></script>
+
 
 
 <!--- Style css -->
--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -15,7 +15,6 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-<link rel="stylesheet" href="{{asset('frontend/css/fancybox.css')}}" media="screen">
 
 <!-- Libraries Stylesheet -->
 <link href="{{asset('frontend/lib/animate/animate.min.css')}}" rel="stylesheet">
@@ -24,6 +23,7 @@
 <!-- Customized Bootstrap Stylesheet -->
 
 <link href="{{asset('frontend/css/bootstrap.min.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('frontend/css/fancybox.css')}}" media="screen">
 
 <!--- Style css -->
 @if (App::getLocale() == 'en')
