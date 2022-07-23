393860dcd75d5bfb64e221fef035a352eec33613
ahmedhamada20 <hamada2031995@gmail.com>
2022-07-23 14:03:01 +0200

    edit

--- resources/views/backend/layout/sidebar.blade.php
+++ resources/views/backend/layout/sidebar.blade.php
@@ -134,7 +134,7 @@
                 </a>
                 <ul id="contact" class="collapse" data-parent="#sidebarnav">
                     <li><a href="{{route('contact')}}">جميع الطلبات </a></li>
-                    <li><a href="{{route('product_request')}}">طلبات عرض الاسعار</a></li>
+                    <li><a href="{{route('product_request_all')}}">طلبات عرض الاسعار</a></li>
                 </ul>
             </li>
 
